d163dd38cea3ad5b853efba3b8a1aaf71e9d0429
suminiee <chansu0114@naver.com>
2024-11-06 20:45:11 +0900

    ♻️[refactor] build.gradle 수정

--- src/main/java/org/example/demo/study/repository/storeRepo/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.demo.study.repository.storeRepo;
-
-import org.example.demo.study.domain.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
-}
--- src/main/java/org/example/demo/study/repository/storeRepo/StoreRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package org.example.demo.study.repository.storeRepo;
-
-import java.util.List;
-
-public interface StoreRepositoryCustom {
-    List<Object> dynamicQueryWithBooleanBuilder(String name, Float score);
-}
